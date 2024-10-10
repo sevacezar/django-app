6ee9e1d18d1edabee3cbbf1cf0007b85b3ab5087
Vsevolod Kryazhev <kryazhev_seva@mail.ru>
2024-10-10 12:26:12 +0500

    Configure for deployment

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -9,28 +9,35 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import logging.config
+from os import getenv
 from pathlib import Path
+import logging
 
 from django.urls import reverse_lazy
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+DATABASE_DIR = BASE_DIR / 'database'
+DATABASE_DIR.mkdir(exist_ok=True)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-bkzz3a%7h%lov!!cj)c-$ehwl2ps5nr#%(vmg_1cmcz-iac(vh"
+SECRET_KEY = getenv(
+    'DJANGO_SECRET_KEY',
+    "django-insecure-bkzz3a%7h%lov!!cj)c-$ehwl2ps5nr#%(vmg_1cmcz-iac(vh",
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = getenv('DJANGO_DEBUG', '0') == '1'
 
 ALLOWED_HOSTS = [
     '0.0.0.0',
     '127.0.0.1',
-]
+] + getenv('DJANGO_ALLOWED_HOSTS', "").split(',')
 INTERNAL_IPS = [
     '127.0.0.1',
 ]
@@ -118,7 +125,7 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": DATABASE_DIR / "db.sqlite3",
     }
 }
 
@@ -231,38 +238,65 @@ SPECTACULAR_SETTINGS = {
 #     }
 # }
 
-LOGFILE_NAME = BASE_DIR / 'log.txt'
-LOGFILE_SIZE = 1 * 1024 * 1024
-# LOGFILE_SIZE = 400
-LOGFILE_COUNT = 3  # сколько логов хранить (помимо текущего)
+# LOGFILE_NAME = BASE_DIR / 'log.txt'
+# LOGFILE_SIZE = 1 * 1024 * 1024
+# # LOGFILE_SIZE = 400
+# LOGFILE_COUNT = 3  # сколько логов хранить (помимо текущего)
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s',
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': False,
+#     'formatters': {
+#         'verbose': {
+#             'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s',
+#         },
+#     },
+#     'handlers': {
+#         'console': {
+#             'class': 'logging.StreamHandler',
+#             'formatter': 'verbose',
+#         },
+#         'logfile': {
+#             'class': 'logging.handlers.RotatingFileHandler',  # ротацция по размеру
+#             # 'class': 'logging.handlers.TimeRotatingFileHandler',  # ротакция по дням
+#             'filename': LOGFILE_NAME,
+#             'maxBytes': LOGFILE_SIZE,
+#             'backupCount': LOGFILE_COUNT,
+#             'formatter': 'verbose'
+#         },
+#     },
+#     'root': {
+#         'handlers': [
+#             'console',
+#             'logfile',
+#         ],
+#         'level': 'DEBUG',
+#     },
+# }
+
+LOGLEVEL = getenv('DJANGO_LOGLEVEL', 'info').upper()
+logging.config.dictConfig(
+    {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'console': {
+                'format': '%(asctime)s %(levelname)s [%(name)s:%(leneno)s] %(module)s %(message)s',
+            },
         },
-    },
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-            'formatter': 'verbose',
+        'handlers': {
+            'console': {
+                'class': 'logging.StreamHandler',
+                'formatter': 'console',
+            },
         },
-        'logfile': {
-            'class': 'logging.handlers.RotatingFileHandler',  # ротацция по размеру
-            # 'class': 'logging.handlers.TimeRotatingFileHandler',  # ротакция по дням
-            'filename': LOGFILE_NAME,
-            'maxBytes': LOGFILE_SIZE,
-            'backupCount': LOGFILE_COUNT,
-            'formatter': 'verbose'
+        'loggers': {
+            '': {
+                'level': LOGLEVEL,
+                'handlers': [
+                    'console',
+                ],
+            },
         },
-    },
-    'root': {
-        'handlers': [
-            'console',
-            'logfile',
-        ],
-        'level': 'DEBUG',
-    },
-}
+    }
+)
